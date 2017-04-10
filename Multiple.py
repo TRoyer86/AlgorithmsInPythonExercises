034d3a0de582c0268b29906ca508f19181ad431d
TRoyer86 <tatyana.royer@gmail.com>
2017-04-10 14:59:45 -0500

    Add files via upload

--- Multiple.py
+++ Multiple.py
@@ -1,3 +1,13 @@
+'''
+Data Structures and Algorithms in Python by Goodrich
+--------------
+Exercise R-1.1
+--------------
+Write a short Python function, is_multiple(n, m) 
+that takes two integer values and returns True 
+if n is a multiple of m.
+'''
+
 def is_multiple(n, m):
     for i in range(1, n+1):
         if n == i * m:
@@ -7,4 +17,3 @@ def is_multiple(n, m):
 if __name__ == '__main__':
     print(is_multiple(10,2))
     print(is_multiple(21, 4))
-    
